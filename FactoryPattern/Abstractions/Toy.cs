e5810e1d7d9618209eb6c0ba36611985db89fa62
R82MXL <balazs.ronto@stud.uni-corvinus.hu>
2020-11-22 17:55:55 +0100

    Jatek absztrakcio kezelese

--- FactoryPattern/Abstractions/Toy.cs
+++ FactoryPattern/Abstractions/Toy.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 
-namespace FactoryPattern.Entities
+namespace FactoryPattern.Abstractions
 {
-    public class Ball : Label
+    public abstract class Toy : Label
     {
-        public Ball()
+        public Toy()
         {
             AutoSize = false;
             Width = 50;
             Height = Width;
-            Paint += Ball_Paint;
+            Paint += Toy_Paint;
         }
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
+        private void Toy_Paint(object sender, PaintEventArgs e)
         {
             DrawImage(e.Graphics);
         }
@@ -28,7 +28,7 @@ namespace FactoryPattern.Entities
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
+        public void MoveToy()
         {
             Left += 1;
         }
